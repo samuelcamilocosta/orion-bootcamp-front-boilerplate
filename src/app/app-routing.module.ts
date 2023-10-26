e51fb810d0171648d5764840a6868a4241805b97
Fabrício Linden <flindenm@hotmail.com>
2023-10-26 13:58:28 -0300

    feat(module.ts): added new route path and some new imports on app.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginPageComponent } from './login-page/login-page.component';
-
+import { PasswordRecoveryPageComponent } from './password-recovery-page/password-recovery-page.component';
 
 const routes: Routes = [
   { path: '', component: LoginPageComponent },
+  { path: 'recovery', component: PasswordRecoveryPageComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,31 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 
+import { NgIf } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatFormFieldModule } from '@angular/material/form-field';
+import { PasswordRecoveryDialogComponent } from './components/password-recovery-dialog/password-recovery-dialog.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { PasswordRecoveryPageComponent } from './password-recovery-page/password-recovery-page.component';
+import { MaterialModule } from './shared/material/material.module';
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    LoginPageComponent,
+    PasswordRecoveryPageComponent,
+    PasswordRecoveryDialogComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    FormsModule,
-    MatFormFieldModule,
+    MaterialModule,
+    ReactiveFormsModule,
+    NgIf,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
