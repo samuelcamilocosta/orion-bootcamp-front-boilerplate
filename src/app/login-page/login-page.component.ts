78a80c97099b7a98f6f17115c9e9e1f8ce0302d0
Fabrício Linden <flindenm@hotmail.com>
2023-10-26 14:09:17 -0300

    feat(login-page): refactor and integration with api

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,57 +1,51 @@
 import { Component } from '@angular/core';
-import {
-  Validators,
-  FormsModule,
-  ReactiveFormsModule,
-  FormGroup,
-  FormBuilder,
-} from '@angular/forms';
-
-import { NgIf, JsonPipe } from '@angular/common';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatDividerModule } from '@angular/material/divider';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ApiV1Service } from 'src/app/api/v1/login.service';
+import { ILoginParams } from 'src/app/interfaces/login-params.interface';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.scss'],
-  standalone: true,
-  imports: [
-    MatFormFieldModule,
-    MatInputModule,
-    MatButtonModule,
-    MatIconModule,
-    FormsModule,
-    ReactiveFormsModule,
-    NgIf,
-    MatCheckboxModule,
-    JsonPipe,
-    MatDividerModule,
-  ],
 })
 export class LoginPageComponent {
   hide = true;
   formGroup: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private loginService: ApiV1Service) {
     this.formGroup = this.fb.group({
-      email: ['', [Validators.required, Validators.email]],
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.email,
+          Validators.pattern(
+            /[a-z0-9!#$%&'*+=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/im
+          ),
+        ],
+      ],
       password: [
         '',
         [
           Validators.required,
-          Validators.pattern(/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%&]).{8,}$/)
-        ]
-      ]
-    })
+          Validators.pattern(
+            /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%&]).{8,}$/
+          ),
+        ],
+      ],
+    });
   }
 
-    isLoginButtonDisabled(): boolean {
+  isLoginButtonDisabled(): boolean {
     return this.formGroup.invalid;
   }
 
+  onSubmit() {
+    if (this.formGroup.valid) {
+      console.log(this.formGroup.value);
+      const data = this.formGroup.value as ILoginParams;
+
+      this.loginService.sendData(data);
+    }
+  }
 }
