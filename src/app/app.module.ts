6a9deaa2f031315726af6592259c71bfd4925265
Fabrício Linden <flindenm@hotmail.com>
2023-10-27 18:40:30 -0300

    feat(app.module): added new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,20 @@ import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ErrorDialogComponent } from './components/error-dialog/error-dialog.component';
 import { PasswordRecoveryDialogComponent } from './components/password-recovery-dialog/password-recovery-dialog.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { PasswordRecoveryPageComponent } from './password-recovery-page/password-recovery-page.component';
 import { MaterialModule } from './shared/material/material.module';
+// import { TestPageComponent } from './test-page/test-page.component';
 @NgModule({
   declarations: [
     AppComponent,
     LoginPageComponent,
     PasswordRecoveryPageComponent,
     PasswordRecoveryDialogComponent,
+    ErrorDialogComponent,
+    // TestPageComponent,
   ],
   imports: [
     BrowserModule,
