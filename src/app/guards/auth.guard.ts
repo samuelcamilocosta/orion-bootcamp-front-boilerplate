996877682b1e7806a33ad2532741b02dece3e5a4
Fabrício Linden <flindenm@hotmail.com>
2023-11-01 17:12:24 -0300

    feat: changed route guard to class based

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,19 +1,26 @@
-import { inject } from '@angular/core';
-import { CanActivateFn, Router } from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+} from '@angular/router';
 import { AuthService } from '../services/auth/auth.service';
 
 /**
  * A route guard function to protect routes.
  */
-export const AuthGuard: CanActivateFn = () => {
-  const user = inject(AuthService);
-  const router = inject(Router);
+export class AuthGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
 
-  if (user.isAuthenticated()) {
-    return true;
-  } else {
-    router.navigate(['']); // Redirect to the login-page route when authentication fails.
-
-    return false;
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): boolean {
+    if (this.authService.isAuthenticated()) {
+      return true;
+    } else {
+      this.router.navigate(['']);
+      return false;
+    }
   }
-};
+}
