7a1e05050479f0deb13d65dbf7989f62992c075e
Fabrício Linden <flindenm@hotmail.com>
2023-11-07 17:45:20 -0300

    feat(component): created meteorology carousel card

---
+++ src/app/shared/components/meteorology-carousel-card/meteorology-carousel-card.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carousel-card',
+  templateUrl: './meteorology-carousel-card.component.html',
+  styleUrls: ['./meteorology-carousel-card.component.scss'],
+})
+export class MeteorologyCarouselCardComponent {}
