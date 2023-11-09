f8def1e504d1b79d2f4ba94b11f040d3e397cc8c
Fabrício Linden <flindenm@hotmail.com>
2023-11-09 15:34:37 -0300

    refactor

--- src/app/api/v1/recovery.service.ts
+++ src/app/api/v1/recovery.service.ts
@@ -2,101 +2,61 @@ import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { Router } from '@angular/router';
-import { ApiV1Service } from 'src/app/api/v1/login.service';
 import { IEmail } from 'src/app/interfaces/recovery-params';
-import { PasswordRecoveryDialogComponent } from 'src/app/shared/components/password-recovery-dialog/password-recovery-dialog.component';
 import { environment } from 'src/environment/environment';
+import { BaseMethods } from './base-methods';
 
 @Injectable({
   providedIn: 'root',
 })
 /**
- * Service that handles the recovery page request
+ * Service that handles the recovery page API request
  */
-export class RecoveryService {
+export class RecoveryService extends BaseMethods {
   private readonly apiURL: string;
 
   /**
    * Constructor *\
-   *
-   * @param fb - An instance of FormBuilder for form creation.
-   * @param loginService - An instance of the ApiV1Service for login-related API calls.
-   * @param auth - An instance of the AuthService for authentication.
+   * @param http - The HttpClient service for making HTTP requests.
    * @param route - An instance of the Router for navigation.
+   * @param dialog - Instance of BaseMethods class for displaying error dialogs.
    */
   constructor(
     private http: HttpClient,
-    private dialog: MatDialog,
     private route: Router,
-    private apiV1Service: ApiV1Service
+    dialog: MatDialog
   ) {
     /**
-     * Initializes the apiURL path
+     * Initializes the apiURL path and dialog
      */
-    this.apiURL = `${environment.api}/v1/recovery`;
-  }
+    super(dialog);
 
-  /**
-   * openDialog
-   *
-   * opens a dialog message for better user experience
-   */
-  openDialog(): void {
-    this.dialog.open(PasswordRecoveryDialogComponent);
+    this.apiURL = `${environment.api}/v1/recovery`;
   }
 
   /**
    * sendEmail
    *
-   * Sends recovery password data to the API and handles the response.
+   * Sends email recovery data to the API and handles the response.
    *
-   * @param email the email data to send
-   * @returns A promise that resolves with the login response data.
+   * @param email - The email to send for recovery.
+   * @returns A Promise that resolves when the data is successfully sent.
+   * @throws When an error occurs during the process.
    */
-  // async sendEmail(email: IEmail): Promise<HttpErrorResponse> {
-  //   return new Promise<HttpErrorResponse>((resolve, reject) => {
-  //     const request = this.http
-  //       .post<HttpErrorResponse>(this.apiURL, email)
-  //       .toPromise();
-
-  //     request
-  //       .then((response) => {
-  //         this.route.navigate(['/']);
-  //         setTimeout(() => {
-  //           this.openDialog();
-  //         }, 500);
-
-  //         resolve(response);
-  //       })
-  //       .catch((error: HttpErrorResponse) => {
-  //         if (error.status === 400 || error.status === 200) {
-  //           this.route.navigate(['/']);
-  //           setTimeout(() => {
-  //             this.openDialog();
-  //           }, 500);
-  //         } else {
-  //           this.apiV1Service.handleError(error);
-  //         }
-
-  //         reject(error);
-  //       });
-  //   });
-  // }
-
-  async sendEmail(email: IEmail) {
+  async sendEmail(email: IEmail): Promise<void> {
     try {
-      const response = await this.http
-        .post<HttpErrorResponse>(this.apiURL, email)
-        .toPromise();
-      // The HTTP request was successful, handle the response as needed
+      await this.http.post(this.apiURL, email).toPromise();
+
       this.route.navigate(['/']);
       setTimeout(() => {
-        this.openDialog();
+        this.openSuccessesDialog();
       }, 500);
-      return response;
     } catch (error) {
-      console.log(error);
-      return null;
+      if (error instanceof HttpErrorResponse) {
+        this.handleError(error);
+      } else {
+        throw error;
+      }
     }
   }
 }
