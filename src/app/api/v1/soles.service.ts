9b24edca73c6959a98fae8371ad54f2d3eba8282
Fabrício Linden <flindenm@hotmail.com>
2023-11-13 09:52:10 -0300

    feat(service): added soles service

--- src/app/api/v1/soles.service.ts
+++ src/app/api/v1/soles.service.ts
@@ -8,14 +8,31 @@ import { BaseMethods } from './base-methods';
 @Injectable({
   providedIn: 'root',
 })
+
+/**
+ * Service that handles the get http request from meteorology page
+ */
 export class SolesService extends BaseMethods {
   private readonly apiUrl: string;
 
+  /**
+   * constructor
+   *
+   * @param http - The HttpClient service for making HTTP requests.
+   * @param dialog - Instance of BaseMethod class for displaying error dialogs.
+   */
   constructor(private http: HttpClient, dialog: MatDialog) {
     super(dialog);
     this.apiUrl = `${environment.api}/v1/soles`;
   }
 
+  /**
+   * getData
+   *
+   * handles the http GET request response and error from the API
+   *
+   * @returns A promise that resolves with the soles response data.
+   */
   async getData(): Promise<ISolesDataInterface[]> {
     return new Promise<ISolesDataInterface[]>((resolve, reject) => {
       const request = this.http
