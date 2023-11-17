2b7b4ff6104925e294f7390622c8263b1b95b095
Fabrício Linden <flindenm@hotmail.com>
2023-11-17 10:13:24 -0300

    feat(delete) denecessary component

--- src/app/shared/components/premium-logo/premium-logo.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-premium-logo',
-  templateUrl: './premium-logo.component.html',
-  styleUrls: ['./premium-logo.component.scss'],
-})
-export class PremiumLogoComponent {}
