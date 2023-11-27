04fece0332c7ca362116b8ef0581f81f8756e258
Fabrício Linden <flindenm@hotmail.com>
2023-11-27 11:49:11 -0300

    docs

--- src/app/guards/premium.guard.ts
+++ src/app/guards/premium.guard.ts
@@ -6,9 +6,29 @@ import { AuthService } from '../services/auth/auth.service';
 @Injectable({
   providedIn: 'root',
 })
+
+/**
+ * PremiumGuard
+ *
+ * class that implements CanActivate to check if a user has premium access.
+ */
 export class PremiumGuard implements CanActivate {
+  /**
+   * constructor
+   *
+   * @param authService: A service responsible for verifying user authentication through local/session storage user data.
+   * @param router: An instance of the Router for navigation.
+   */
   constructor(private authService: AuthService, private router: Router) {}
 
+  /**
+   * CanActivate
+   *
+   * method to determine if the route can be activated.
+   *
+   * @returns 'true' if the user has premium access, otherwise navigates to the home page
+   * and returns 'false'.
+   */
   canActivate(): boolean {
     if (this.authService.isPremium()) {
       return true;
