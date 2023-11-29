0b85e2672040b460dbc985cc6be4818df3010e9c
Fabrício Linden <flindenm@hotmail.com>
2023-11-29 11:53:16 -0300

    feat(component): created new password page

---
+++ src/app/pages/change-password-page/change-password-page.component.ts
@@ -0,0 +1,179 @@
+import { Component } from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
+
+@Component({
+  selector: 'app-change-password-page',
+  templateUrl: './change-password-page.component.html',
+  styleUrls: ['./change-password-page.component.scss'],
+})
+export class ChangePasswordPageComponent {
+  /**
+   * hide/hideConfirmPassword: Controls whether the password input is hidden or shown.
+   */
+  hide = true;
+  hideConfirmPassword = true;
+
+  /**
+   * Form group for sign up page
+   */
+  changePasswordForm: FormGroup;
+
+  /**
+   * Constructor
+   *
+   * @param fb - An instance of FormBuilder for form creation.
+   */
+  constructor(private fb: FormBuilder) {
+    // Initialize the formGroup with email, password, and checkbox fields.
+    this.changePasswordForm = this.fb.group(
+      {
+        password: [
+          '',
+          [
+            Validators.required,
+            Validators.pattern(
+              /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%&]).{8,}$/
+            ),
+          ],
+        ],
+        confirmPassword: ['', [Validators.required]],
+      },
+      { validator: this.passwordMatchValidator }
+    );
+  }
+
+  /**
+   * Handles form submission by sending data to the login service.
+   */
+  async onSubmit() {
+    const formData = this.changePasswordForm.value;
+    const dataToSend = {
+      name: formData.name,
+      email: formData.email,
+      password: formData.password,
+    };
+
+    // await this.changePasswordService.sendData(dataToSend);
+  }
+
+  /**
+   * passwordMatchValidator
+   *
+   * custom validator to check if password and confirmPassword are identical
+   *
+   * @param control The form group to validate.
+   * @returns A validation error object if the passwords don't match, otherwise null.
+   */
+  private passwordMatchValidator: ValidatorFn = (
+    control: AbstractControl
+  ): { [key: string]: boolean } | null => {
+    const password = control.get('password');
+    const confirmPassword = control.get('confirmPassword');
+
+    if (password?.value.length > 0 || confirmPassword?.value.length > 0) {
+      if (
+        password &&
+        confirmPassword &&
+        password.value !== confirmPassword.value
+      ) {
+        confirmPassword.setErrors({ passwordMismatch: true });
+        return { passwordMismatch: true };
+      } else {
+        confirmPassword?.setErrors(null);
+        return null;
+      }
+    }
+    return null;
+  };
+
+  /**
+   * showCheck
+   *
+   * method to apply visibility style based on condition met
+   * @returns 'hidden' if true, 'visible' otherwise
+   */
+  showCheck() {
+    return this.changePasswordForm.get('password')?.value.length === 0
+      ? 'hidden'
+      : 'visible';
+  }
+
+  /**
+   * lengthCheck
+   *
+   * method to dynamically apply class based on condition
+   * @returns 'validPassword' if true, 'invalidPassword' otherwise
+   */
+  lengthCheck(): string | undefined {
+    const passwordLength =
+      this.changePasswordForm.get('password')?.value.length;
+
+    if (passwordLength === 0) {
+      return undefined;
+    } else {
+      return passwordLength >= 8 ? 'validPassword' : 'invalidPassword';
+    }
+  }
+
+  /**
+   * checkPattern
+   *
+   * method to handle regex tests
+   *
+   * @param regex parameter @type {RegExp} to be received
+   * @returns 'validPassword' if true, 'invalidPassword' otherwise
+   */
+  private checkPattern(regex: RegExp): string | undefined {
+    const passwordValue = this.changePasswordForm.get('password')?.value;
+
+    if (passwordValue.length > 0) {
+      return regex.test(passwordValue) ? 'validPassword' : 'invalidPassword';
+    }
+    return undefined;
+  }
+
+  /**
+   * lengthCheck
+   *
+   * method to dynamically apply class based on condition
+   * @returns 'validPassword' if true, 'invalidPassword' otherwise
+   */
+  letterCheck(): string | undefined {
+    const passwordLength =
+      this.changePasswordForm.get('password')?.value.length;
+
+    if (passwordLength > 0) {
+      const hasUpperCase = this.checkPattern(/[A-Z]/) === 'validPassword';
+      const hasLowerCase = this.checkPattern(/[a-z]/) === 'validPassword';
+
+      return hasUpperCase && hasLowerCase ? 'validPassword' : 'invalidPassword';
+    }
+    return undefined;
+  }
+
+  /**
+   * numberCheck
+   *
+   * method to dynamically apply class based on condition
+   * @returns 'validPassword' if true, 'invalidPassword' otherwise
+   */
+  numberCheck(): string | undefined {
+    return this.checkPattern(/\d/);
+  }
+
+  /**
+   * specialCharacterCheck
+   *
+   * method to dynamically apply class based on condition
+   * @returns 'validPassword' if true, 'invalidPassword' otherwise
+   */
+  specialCharacterCheck(): string | undefined {
+    return this.checkPattern(/[!@#$%&]/);
+  }
+}
